969410ebc4d8aadd5b803fc33e59e37f820e914d
Minh Nguyen <hongminh1620@gmail.com>
2021-10-29 16:42:55 +0100

    Add path login, signup, chatbot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@ahaui/css/dist/index.min.css";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Chatbot from "react-chatbot-kit";
+
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import ActionProvider from "./components/ActionProvider";
+import MessageParser from "./components/MessageParser";
+import config from "./config"
 
 function App() {
+  // Signup
+  const signup = async (body) => {
+    console.log(body);
+    const res = await fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+
+  // Login
+  const login = async (body) => {
+    console.log(body);
+    const res = await fetch("http://localhost:5000/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/signup">
+          <Signup postUsers={signup} />
+        </Route>
+        <Route exact path="/login">
+          <Login postAuth={login} />
+        </Route>
+        <Route exact path="/wizaid">
+          <Chatbot
+            config={config}
+            actionProvider={ActionProvider}
+            messageParser={MessageParser}
+          />
+        </Route>
+      </div>
+    </Router>
   );
 }
 

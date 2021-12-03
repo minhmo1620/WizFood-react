2590d6851b6e26cb2f6ce351484fb1930e22fd77
Minh Nguyen <hongminh1620@gmail.com>
2021-12-03 23:09:47 +0000

    Add components for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,104 @@
-import React from "react";
-import {Button} from "@ahaui/react";
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import { Form, Button } from "@ahaui/react";
 
-import {deleteTokens} from '../auth'
+import { deleteTokens } from "../auth";
 
+export default function Home() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [boxes, setBoxes] = useState([]);
 
-class Home extends React.Component {
-    handleLogOut = () => {
-        deleteTokens();
-    };
-    render() {
-        return (
-            <div>
-                <h3>Logged in as {localStorage.getItem("username")}</h3>
-                <Button onClick={() => this.handleLogOut()}>Logout</Button>
-            </div>
-        )
-    }
+  const history = useHistory();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/boxes`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setBoxes(data);
+      })
+      .catch((err) => console.log(err));
+  });
+
+  // Create a new box
+  const createNewBox = async (body) => {
+    console.log(body);
+    fetch("http://localhost:5000/boxes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        history.push("/boxes/" + data.id);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createNewBox({ name, description });
+
+    setName("");
+    setDescription("");
+  };
+
+  const handleLogOut = () => {
+    deleteTokens();
+  };
+
+  const goToBox = (box_id) => {
+      history.push('/boxes/'+ box_id );
+  }
+
+  return (
+    <div>
+      <h3>Logged in as {localStorage.getItem("username")}</h3>
+      <Button onClick={() => handleLogOut()}>Logout</Button>
+      <Link to="/about">
+        <Button>About</Button>
+      </Link>
+      <Link to="/wizaid">
+        <Button>WizAId</Button>
+      </Link>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="name">
+          <Form.Input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Event's name"
+          />
+        </Form.Group>
+        <Form.Group controlId="description">
+          <Form.Input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Event's description"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          <Button.Label>Create a new Wizbox</Button.Label>
+        </Button>
+      </Form>
+      <p>Boxes</p>
+      {boxes.map((box) => (
+        <div key={box.id}>
+          <Button onClick={() => goToBox(box.id)}>Box {box.id}</Button>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default Home

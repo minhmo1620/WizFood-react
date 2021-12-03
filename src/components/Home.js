896ed262e4572eec035811f237eae28304b28876
Minh Nguyen <hongminh1620@gmail.com>
2021-12-03 23:35:26 +0000

    fix: Fix useEffect spam GET request

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,7 +24,7 @@ export default function Home() {
         setBoxes(data);
       })
       .catch((err) => console.log(err));
-  });
+  }, []);
 
   // Create a new box
   const createNewBox = async (body) => {

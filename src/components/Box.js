e14c033f29d509771bc3a60c1faa57231b413446
Minh Nguyen <hongminh1620@gmail.com>
2021-12-03 23:10:13 +0000

    Box components for one box

---
+++ src/components/Box.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router";
+import { Button } from "@ahaui/react";
+
+export default function Box(props) {
+  const { box_id } = useParams();
+  const [data, setData] = useState(null);
+  const [options, setOptions] = useState([]);
+  const [sum_votes, setSumVotes] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/boxes/${box_id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+
+    fetch(`http://localhost:5000/boxes/${box_id}/options`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setOptions(data);
+        let sum_votes = {};
+        data.forEach((option) => {
+          sum_votes[option.id] =
+            option.vote[0] + option.vote[1] + option.vote[2];
+        });
+        setSumVotes(sum_votes);
+      })
+      .catch((err) => console.log(err));
+  }, [box_id]);
+
+  if (data === null) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        Back
+      </Button>
+      <Button
+        onClick={() => {
+          history.push(`/boxes/${box_id}/create`);
+        }}
+      >
+        Add
+      </Button>
+      <Button
+        onClick={() => {
+          history.push(`/boxes/${box_id}/vote`);
+        }}
+      >
+        Vote
+      </Button>
+      <h2>{data.id}</h2>
+      <p>{data.name}</p>
+      <p>{data.description}</p>
+      <p>Options</p>
+      {options.map((option) => (
+        <div key={option.id}>
+          <div>ID: {option.id}</div>
+          <div>Name: {option.name}</div>
+          <div>Description: {option.description}</div>
+          <div>
+            {" "}
+            Happy: {option.vote[0]}/{sum_votes[option.id]}
+          </div>
+          <div>
+            {" "}
+            Neutral: {option.vote[1]}/{sum_votes[option.id]}
+          </div>
+          <div>
+            {" "}
+            Sad: {option.vote[2]}/{sum_votes[option.id]}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}

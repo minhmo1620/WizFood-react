8924531e53a8f93359da04ed4678d97ca4171d83
Minh Nguyen <hongminh1620@gmail.com>
2022-04-01 11:35:29 -0700

    Modify Box Component

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router";
-import { Button } from "@ahaui/react";
+import { Button, Icon } from "@ahaui/react";
+import MainHeader from "./Headers/MainHeader";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_HOST;
 
@@ -17,7 +18,7 @@ export default function Box(props) {
     Happy: `\u{1F600}`,
     Neutral: `\u{1F610}`,
     Sad: `\u{1F914}`,
-  }
+  };
   Object.entries(emoji).map(([key, value]) => {
     const e = value.match(regex)[0];
     value = e;
@@ -73,53 +74,74 @@ export default function Box(props) {
 
   return (
     <div>
-      <div>
-        <Button
-          onClick={() => {
-            history.push("/");
-          }}
-        >
-          Back
-        </Button>
-        <Button
-          onClick={() => {
-            history.push(`/boxes/${box_id}/create`);
-          }}
-        >
-          Add
-        </Button>
-        <Button
-          onClick={() => {
-            history.push(`/boxes/${box_id}/vote`);
-          }}
-        >
-          Vote
-        </Button>
-      </div>
-      <h2>Box ID: {data.id}</h2>
-      <h3>Name: {data.name}</h3>
-      <h4>Description: {data.description}</h4>
-      <h5>Available options</h5>
-      {options.map((option) => (
-        <div key={option.id}>
-          <p></p>
-          <div>Option's ID: {option.id}</div>
-          <div>Name: {option.name}</div>
-          <div>Description: {option.description}</div>
-          <div>
-            {" "}
-            {emoji.Happy} : {option.vote[0]}/{sum_votes[option.id]}
+      <MainHeader />
+      <div className="Grid u-marginBottomSmall">
+        <div className="u-sizeFull lg:u-size1of12"></div>
+        <div className="u-sizeFull lg:u-size10of12">
+          <div className="u-flex u-flexRow b-highlight u-marginBottomSmall">
+            <div className="u-paddingExtraSmall b-highlight">
+              <div className="u-textLeft u-text800">Box ID: {data.id}</div>
+              <div className="u-textLeft u-text300">{data.name}</div>
+              <div className="u-textLeft u-text300">{data.description}</div>
+            </div>
+            <div className="u-paddingExtraSmall b-highlight"></div>
+            <div
+              className="u-paddingExtraSmall b-highlight"
+              style={{ marginLeft: "30%" }}
+            >
+              <Button
+                onClick={() => {
+                  history.push(`/boxes/${box_id}/create`);
+                }}
+                className="u-backgroundAccentLight"
+              >
+                <Icon name="plus" size="small" className="u-textPrimary" />
+                <Button.Label className="u-paddingExtraSmall u-textCenter u-textBlack">
+                  Add new option
+                </Button.Label>
+              </Button>
+            </div>
+            <div
+              className="u-paddingExtraSmall b-highlight"
+              style={{ marginLeft: "7%" }}
+            >
+              <Button
+                onClick={() => {
+                  history.push(`/boxes/${box_id}/vote`);
+                }}
+                className="u-backgroundAccentLight"
+              >
+                <Icon name="checkmark" size="small" className="u-textPrimary" />
+                <Button.Label className="u-paddingExtraSmall u-textCenter u-textBlack">
+                  Vote
+                </Button.Label>
+              </Button>
+            </div>
           </div>
-          <div>
-            {" "}
-            {emoji.Neutral} : {option.vote[1]}/{sum_votes[option.id]}
-          </div>
-          <div>
-            {" "}
-            {emoji.Sad} : {option.vote[2]}/{sum_votes[option.id]}
+          <div className="u-text500">Available options</div>
+          <div className="u-flex u-flexColumn b-highlight u-marginBottomSmall">
+            {options.map((option) => (
+              <div key={option.id} className="u-paddingExtraSmall b-highlight">
+                <div>Name: {option.name}</div>
+                <div>Description: {option.description}</div>
+                <div>
+                  {" "}
+                  {emoji.Happy} : {option.vote[0]}/{sum_votes[option.id]}
+                </div>
+                <div>
+                  {" "}
+                  {emoji.Neutral} : {option.vote[1]}/{sum_votes[option.id]}
+                </div>
+                <div>
+                  {" "}
+                  {emoji.Sad} : {option.vote[2]}/{sum_votes[option.id]}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      ))}
+        <div className="u-sizeFull lg:u-size1of12"></div>
+      </div>
     </div>
   );
 }
